215e5c47dfa6f89f756eeddc3809f378162b65af
Diana <dianagastrin@hotmail.com>
2016-03-05 21:10:15 +0200

    clean works in highly precentage. NOT FINISHED YET. need to save as pickle the clean data.

--- wikiParserBiography.py
+++ wikiParserBiography.py
@@ -4,45 +4,120 @@ import re
 from bs4 import BeautifulSoup
 import pickle
 import string
-
-
 def remove_accents(input_str):
     nfkd_form = unicodedata.normalize('NFKD', input_str)
     only_ascii = nfkd_form.encode('ASCII', 'ignore')
     return only_ascii.decode('utf-8')
-
 # global variables
-templateA = re.compile("\{\{([a-z,A-Z,0-9]|\s|=|-)+\|[a-z,A-Z,0-9,_]+([a-z,A-Z,0-9]|\s|=)+\}\}")
+templateA = re.compile("{\{([a-z,A-Z,0-9,_]|\s|=|-)+\|[a-z,A-Z,0-9,_]+([a-z,A-Z,0-9]|\s|=)+\}\}")# :example {{kd-ds|dsd}}
 templateB = re.compile("\[\[([a-z,A-Z,0-9]|\s|=)+\|[a-z,A-Z,0-9]+([a-z,A-Z,0-9]|\s|=)+\]\]")
 templateC = re.compile("\{\{([a-z,A-Z,0-9]|\s|=|-)+\|([a-z,A-Z,0-9]|\s|=|-)+\|([a-z,A-Z,0-9]|\s|=|-)+\}\}")
 templateD = re.compile("([a-z,A-Z,0-9]|\s|\(|\))+\|([a-z,A-Z,0-9]|\s|\(|\)|=)+")
 templateE = re.compile("\[\[([a-z,A-Z,0-9]|\s|=)+:[a-z,A-Z,0-9]+([a-z,A-Z,0-9]|\s|=|_)+\]\]")
 templateF = re.compile("\*.+")
 templateG = re.compile("\{\|(_ | a-z |A-Z |0-9 |\w)+\|\}")
-templateH = re.compile(r"\{\{infobox[\s,a-z,A-Z,0-9,_,\|]+\|\}\}")
-# templateI = re.compile("\{\{.*\}\}") #example {{anyStr}}
-templateJ = re.compile("(\(|\[)(\)|\])")  # exampe () or []
+templateH = re.compile(r"\{\{(Infobox|infobox)[\:,\s,a-z,A-Z,0-9,_,\|]+\|\}\}") #{Infobox anyStr |}}
+templateI = re.compile("\{\|[\s,a-z,A-Z,0-9,\=,\',_,\-,\,,\",\|,\!,\;,\.,\),\(,\W]*\|\}")#: {| any Str |}
+templateJ = re.compile("(\(|\[)(\)|\])")# exampe () or []
 match_urls = re.compile(r"""((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.‌​][a-z]{2,4}/)(?:[^\s()<>]+|(([^\s()<>]+|(([^\s()<>]+)))*))+(?:(([^\s()<>]+|(‌​([^\s()<>]+)))*)|[^\s`!()[]{};:'".,<>?«»“”‘’]))""",re.DOTALL)
-match_file_or_image = re.compile("File:|Image:")
-get_titles = re.compile("\=\=([\s,a-z,A-Z,0-9]|\s|=|-)+\=\=")  # find titles ==title==
+match_file_or_image = re.compile("\[\[(File:|Image:|image:|file:)[a-z,A-Z,0-9,\|,\-,\=,\',\.,\s,_]+(\[\[[a-z,A-Z,0-9,\|,\-,\=,',\.,\s,_]+\]\])*\]\]")# :example [[file: [[dg]] sdg]]
+get_titles = re.compile("\=\=([\s,a-z,A-Z,0-9]|\s|=|-)+\=\=")#:example ==title==
+get_summary_title = re.compile(":\'\'\'[a-z,A-Z,0-9,\s,=,\-,_,\']+\'\'\'")#:example :''''' someStr '''
+get_unwanted_ref = re.compile("<ref[\s,a-z,A-Z,0-9,=,\',_,\-,\,,\"]+/>")#:example <ref someStr />
 
 defaultTitle = "Biography"
 curpos_title_paragraphs = []
 curpos_wikipedia_biography = []
 
-
 def get_title(page):
     return remove_accents(page.title.get_text())
-
-
 def get_text(page):
     text = clean_text(page.text)
     title = get_title(page)
     get_title_content_paragraph(title, text)
     return text
+def clean_left_tags(text):
+    ref_iter = get_unwanted_ref.finditer(text)
+    for match in ref_iter:
+        text = text.replace(match.group(),'')
+    soup_deeper = BeautifulSoup(text, "lxml")
+    [s.extract() for s in soup_deeper('ref')]
+    [s.extract() for s in soup_deeper('center')]
+    [s.extract() for s in soup_deeper('gallery')]
+    return soup_deeper.text
+def find_templates(text):
+    templ_urls = match_urls.finditer(text)  # example "http://in.bgu.ac.il/Pages/default.aspx"
+    for match in templ_urls:
+        text = text.replace(match.group(), '')
+    temp_file_image = match_file_or_image.finditer(text)
+    for match in temp_file_image:
+        text = text.replace(match.group(), '')
+    templ_iteratorA = templateA.finditer(text)  # example {{str1 | str2}}
+    for match in templ_iteratorA:
+        text = text.replace(match.group(), match.group()[match.group().find('|') + 1:-2])  # not includes }}
 
+    templ_iteratorB = templateB.finditer(text)  # example [[str1 | str2]]
+    for match in templ_iteratorB:
+        text = text.replace(match.group(), match.group()[match.group().find('|') + 1:-2])  # not includes ]]
+    templ_iteratorC = templateC.finditer(text)  # example {{str1|str2|str3}}
+    for match in templ_iteratorC:
+        text = text.replace(match.group(), '')
 
-def get_title_content_paragraph(titleOfArticle, cleanText):
+    templ_iteratorD = templateD.finditer(text)  # example str1|str2
+    for match in templ_iteratorD:
+        text = text.replace(match.group(), match.group()[match.group().find('|') + 1:])
+    templ_iteratorE = templateD.finditer(text)  # example [str1:str]
+    for match in templ_iteratorE:
+        text = text.replace(match.group(), '')
+    templ_iteratorG = templateD.finditer(text)  # example [str1:str]
+    for match in templ_iteratorG:
+        text = text.replace(match.group(), '')
+    templ_iteratorI = templateI.finditer(text) #example {|anySTR|}
+    for match in templ_iteratorI:
+        text = text.replace(match.group(), '')
+    templ_match_file_or_image = match_file_or_image.finditer(text)  # example ()
+    for match in templ_match_file_or_image:
+        text = text.replace(match.group(), '')
+
+    templ_iteratorJ = templateJ.finditer(text) #example () or []
+    for match in templ_iteratorJ:
+        text = text.replace(match.group(), '')
+
+    return text
+def clean_text(text):
+    temp_un_wanted_titles = get_summary_title.finditer(text)
+    for match in temp_un_wanted_titles:
+        text = text.replace(match.group(),'')
+    if text.find('\'\'\'') > 0:
+        text = text[text.find('\'\'\''):]
+        text = text.replace('\'\'\'', '')
+    text = clean_left_tags(text)
+    if text.find('==References==') > 0:
+        text = text[:text.find('==References==')]
+    if text.find('== References ==') > 0:
+        text = text[:text.find('== References ==')]
+    if text.find('==Ancestors==') > 0:
+        text = text[:text.find('==Ancestors==')]
+    if text.find('==Notes==') > 0:
+        text = text[:text.find('==Notes==')]
+    if text.find('== Notes ==') > 0:
+        text = text[:text.find('== Notes ==')]
+    if text.find('==External links==') > 0:
+        text = text[:text.find('==External links==')]
+    if text.find('==Gallery==') > 0:
+        text = text[:text.find('==Gallery==')]
+    if text.find('==Further reading==') > 0:
+        text = text[:text.find('==Further reading==')]
+    if text.find('==See also==') > 0:
+        text = text[:text.find('==See also==')]
+    if text.find('==References‬‏==') > 0:
+        text = text[:text.find('==References‬‏==')]
+    if text.find('== Bibliography ‬‏==') > 0:
+        text = text[:text.find('== Bibliography ‬‏==')]
+    text = find_templates(text)
+    return text.replace(']]', '').replace('[[', '').replace('}}', '').replace('{{', '')
+
+def get_title_content_paragraph(titleOfArticle,cleanText):
     title_paragraph = []
     title_text = []
     titles_of_paragraphs = []
@@ -66,88 +141,7 @@ def get_title_content_paragraph(titleOfArticle, cleanText):
         title_paragraph.append((title, paragraph))
     title_text.append((titleOfArticle, title_paragraph))
     curpos_wikipedia_biography.append(title_text)
-    curpos_title_paragraphs.append(titles_of_paragraphs)
-
-
-def clean_left_tags(text):
-    soup_deeper = BeautifulSoup(text, "lxml")
-    [s.extract() for s in soup_deeper('ref')]
-    [s.extract() for s in soup_deeper('center')]
-    return soup_deeper.text
-
-
-def find_templates(text):
-    templ_urls = match_urls.finditer(text)  # example "http://in.bgu.ac.il/Pages/default.aspx"
-    for match in templ_urls:
-        text = text.replace(match.group(), '')
-    templ_iteratorA = templateA.finditer(text)  # example {{str1 | str2}}
-    for match in templ_iteratorA:
-        text = text.replace(match.group(), match.group()[match.group().find('|') + 1:-2])  # not includes }}
-
-    templ_iteratorB = templateB.finditer(text)  # example [[str1 | str2]]
-    for match in templ_iteratorB:
-        text = text.replace(match.group(), match.group()[match.group().find('|') + 1:-2])  # not includes ]]
-    templ_iteratorC = templateC.finditer(text)  # example {{str1|str2|str3}}
-    for match in templ_iteratorC:
-        text = text.replace(match.group(), '')
-    templ_iteratorC = templateC.finditer(text)  # example {{str1|str2|str3}}
-    for match in templ_iteratorC:
-        text = text.replace(match.group(), '')
-
-    templ_iteratorD = templateD.finditer(text)  # example str1|str2
-    for match in templ_iteratorD:
-        text = text.replace(match.group(), match.group()[match.group().find('|') + 1:])
-    templ_iteratorE = templateD.finditer(text)  # example [str1:str]
-    for match in templ_iteratorE:
-        text = text.replace(match.group(), '')
-    #
-    # templ_iteratorF = templateD.finditer(text) #example [str1:str]
-    # for match in templ_iteratorF:
-    #     text = text.replace(match.group(), '')
-
-    templ_iteratorG = templateD.finditer(text)  # example [str1:str]
-    for match in templ_iteratorG:
-        text = text.replace(match.group(), '')
-    # templ_iteratorI = templateI.finditer(text) #example {{anyStr}}
-    # for match in templ_iteratorI:
-    #     text = text.replace(match.group(), '')
-
-    templ_match_file_or_image = match_file_or_image.finditer(text)  # example ()
-    for match in templ_match_file_or_image:
-        text = text.replace(match.group(), '')
-
-    return text
-
-
-def clean_text(text):
-    if text.find('\'\'\'') > 0:
-        text = text[text.find('\''''):]
-        text = text.replace('\'\'\'', '')
-    text = clean_left_tags(text)
-    if text.find('==References==') > 0:
-        text = text[:text.find('==References==')]
-    if text.find('== References ==') > 0:
-        text = text[:text.find('== References ==')]
-    if text.find('==Ancestors==') > 0:
-        text = text[:text.find('==Ancestors==')]
-    if text.find('==Notes==') > 0:
-        text = text[:text.find('==Notes==')]
-    if text.find('==External links==') > 0:
-        text = text[:text.find('==Notes==')]
-    if text.find('==Gallery==') > 0:
-        text = text[:text.find('==Gallery==')]
-    if text.find('==Further reading==') > 0:
-        text = text[:text.find('==Further reading==')]
-    if text.find('==See also==') > 0:
-        text = text[:text.find('==See also==')]
-    if text.find('==References‬‏==') > 0:
-        text = text[:text.find('==References‬‏==')]
-    if text.find('== Bibliography ‬‏==') > 0:
-        text = text[:text.find('== Bibliography ‬‏==')]
-    text = find_templates(text)
-    text = text.replace("lang-grc|", '')
-    return text.replace(']]', '').replace('[[', '').replace('}}', '').replace('{{', '')
-
+    return curpos_title_paragraphs.append(titles_of_paragraphs)
 
 def most_frequent_titles(listOfTitles):
     pass
@@ -195,5 +189,14 @@ if __name__ == '__main__':
     #dump_biography('enwiki-latest-pages-articles.xml.bz2', 15000)
     TITLE = 0
     TEXT = 1
+    clean_biographies = []
     for i, biography in enumerate(iterator_on_biography()):
-       clean = clean_text(biography[TEXT])
+        clean_biography = clean_text(biography[TEXT])
+        title = biography[0]
+        clean_biographies.append(get_title_content_paragraph(title,clean_biography))
+        if i % 1000:
+            with open('corpus' + str(i) + '.pickle', 'wb') as handle:
+                pickle.dump(clean_biographies, handle)
+            clean_biographies = []
+        print(i)
+
